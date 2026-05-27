17755e4c8843ff2e51d4ad1aa6c1fb71ea1d1ad0
Mandy <amanda.britosilva01@gmail.com>
2026-05-26 22:14:19 -0300

    Lista PII

--- Programação para Internet - PPI/Atividades/listas_de_atividade02/app.js
+++ Programação para Internet - PPI/Atividades/listas_de_atividade02/app.js
@@ -92,11 +92,74 @@ function ex011() {
 
 }
 
-function ex012(){
-   let numero_01 = parseFloat(document.getElementById("inputAltura").value);
-   let numero_02 = parseInt(document.getElementById("inputPeso").value);
-   let resultado = document.getElementById("resultado12")
-   let imc = numero_02/(numero_01*numero_01);
-   resultado.textContent = imc;
+function ex012() {
+  let numero_01 = parseFloat(document.getElementById("inputAltura").value);
+  let numero_02 = parseInt(document.getElementById("inputPeso").value);
+  let resultado = document.getElementById("resultado_12")
+  let imc = numero_02 / (numero_01 * numero_01);
+  resultado.textContent = imc;
 
- }
+}
+
+function ex013() {
+
+  let number_01 = parseInt(document.getElementById("inputFatorial").value);
+  let fatorial = 1;
+  for (let i = 1; i <= number_01; i++) {
+    fatorial *= i;
+
+  }
+
+  let resultado_13 = document.getElementById("resultado_13")
+  resultado_13.textContent = fatorial;
+
+}
+
+function ex014() {
+
+  let number_01 = parseFloat(document.getElementById("inputReais").value);
+
+  let conversao = number_01 * 4.80;
+
+  let resultado_14 = document.getElementById("resultado_14");
+  resultado_14.textContent = conversao
+}
+
+function ex015() {
+
+
+  let altura = parseFloat(document.getElementById("inputAltura").value)
+  let largura = parseFloat(document.getElementById("inputLarg").value)
+
+  let area = altura * largura;
+
+  let perimetro = (altura * 2) + (largura * 2);
+
+  let resultado_15 = document.getElementById("resultado_15");
+  resultado_15.textContent = area;
+
+  let resultado_15_1 = document.getElementById("resultado_15.1");
+  resultado_15_1.textContent = perimetro;
+
+}
+
+
+function ex016() {
+
+
+
+  let raio = parseFloat(document.getElementById("inputArea").value)
+
+  let area = 3.14 * (raio * raio);
+
+  let perimetro = 2 * 3.14 * raio;
+
+  let resultado_16 = document.getElementById("resultado_16");
+  resultado_16.textContent = area;
+
+  let resultado_16_1 = document.getElementById("resultado_16.1");
+  resultado_16_1.textContent = perimetro;
+
+
+}
+
