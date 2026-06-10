e03ccd14c2c1d1f2a63f30785d3354a05b73efd6
Mandy <amanda.britosilva01@gmail.com>
2026-06-10 19:26:57 -0300

    PPI
    Programação Para Internet:
    Compreendendo o código em relação aos cards, Js para mudar os atributos para o site

---
+++ Programação para Internet - PPI/Atividades/web_component/assets/js/dna_cards.js
@@ -0,0 +1,26 @@
+export class DnaCard extends HTMLElement {
+
+  constructor() {
+
+    super();
+
+  }
+
+  connectedCallback() {
+
+    let imagem = this.getAttribute('img');
+    let titulo = this.getAttribute('inputTitle');
+    let conteudo = this.innerHTML;
+    let botao = this.getAttribute('buttom')
+
+    this.innerHTML = `
+    <div class="card" style="width: 18rem;">
+      <img src="${imagem}" alt="">
+      <div class="card-body">
+        <h5 class="card-title">${titulo}</h5>
+        <p class="card-text">${conteudo}</p>
+        <a href="#" class="btn btn-primary">${botao}</a>
+      </div>
+    </div>      `;
+  }
+}
---
+++ Programação para Internet - PPI/Atividades/web_component/assets/js/lib.js
@@ -0,0 +1,5 @@
+import { DnaCard } from "./dna_cards.js";
+customElements.define('dna-cards', DnaCard);
+
+import { DnaNavbar } from "./dna_navbar.js";
+customElements.define('dna-navbar', DnaNavbar);
